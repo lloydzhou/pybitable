c9cce8879542daa5bdce2c243769eb284f48c5f7
lloydzhou <lloydzhou@qq.com>
2024-01-29 19:40:55 +0800

    hotfix for install error

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 """Setup script for Lark BITable."""
+import re
 import setuptools
-from pybitable import __version__
+
+
+with open("pybitable/__init__.py") as f:
+    version = re.findall("__version__.*(\d+\.\d+\.\d+).*", f.read())[0]
 
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
 setuptools.setup(
     name="pybitable",
-    version=__version__,
+    version=version,
     author="lloydzhou",
     author_email="lloydzhou@qq.com",
     url="https://github.com/lloydzhou/pybitable",
